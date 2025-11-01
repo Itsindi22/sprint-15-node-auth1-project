8c97eef34e41a8b9f97cdb2b76e9dbbbd548699f
Inderpreet <inderpreet2004123@gmail.com>
2025-11-01 13:53:29 -0400

    Secret cookie

--- api/server.js
+++ api/server.js
@@ -3,6 +3,10 @@ const helmet = require("helmet");
 const cors = require("cors");
 const usersRouter = require ('./users/users-router');
 const authRouter = require ('./auth/auth-router');
+const session = require ('express-session')
+const Store = require ('connect-session-knex')(session)
+const knex= require('../data/db-config')
+
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -18,7 +22,25 @@ const authRouter = require ('./auth/auth-router');
  */
 
 const server = express();
-
+server.use (session ({
+  name: 'ChoclateChip',
+  secret: 'shh',
+  saveUninitialized: false,
+  resave:false,
+  store: new Store ({
+    knex,
+    createTable: true,
+    clearInterval: 100 * 60 * 10,
+    tablame: 'sessions',
+        sidfieldname: 'sid',
+        }),
+  cookie: {
+    maxAge:1000 *60 *10,
+    secure: false,
+    httpOnly: true,
+    // sameSide: 'none'
+  }
+}))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
